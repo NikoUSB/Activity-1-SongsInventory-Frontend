cfbe0f4b8c9da0bbb7e7abf7a61497f721a920ca
NikoUSB <nalvarados@academia.usbbog.edu.co>
2025-05-18 19:43:06 -0500

    Create Proyect

---
+++ src/test/java/com/usb/SongsInventoryFront/SongsInventoryFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usb.SongsInventoryFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongsInventoryFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
